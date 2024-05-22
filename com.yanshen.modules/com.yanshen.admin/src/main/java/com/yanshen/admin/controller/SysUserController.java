5a63fcefcdeb824a6923c4dc1372351a0d7672b5
崔彦超 <1060846413@qq.com>
2024-05-22 15:43:59 +0800

    initial AdminService

--- com.yanshen.modules/com.yanshen.admin/src/main/java/com/yanshen/admin/controller/SysUserController.java
+++ com.yanshen.modules/com.yanshen.admin/src/main/java/com/yanshen/admin/controller/SysUserController.java
@@ -9,6 +9,8 @@ import com.yanshen.common.core.web.page.PageQuery;
 import com.yanshen.common.log.annotation.Log;
 import com.yanshen.common.log.enums.BusinessType;
 import com.yanshen.common.web.BasePlusController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -28,6 +30,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/sysUser")
+@Api(tags = "用户管理")
 public class SysUserController extends BasePlusController {
 
     private final ISysUserService sysUserService;
@@ -37,6 +40,7 @@ public class SysUserController extends BasePlusController {
      */
     //@SaCheckPermission("")
     @GetMapping("/listPage")
+    @ApiOperation(value = "查询列表-分页")
     public R<Page<SysUserVo>> list(SysUserDto dto, PageQuery pageQuery) {
         Page<SysUserVo> resultPage = sysUserService.queryPageList(dto, pageQuery);
         return R.ok(resultPage);
@@ -47,6 +51,7 @@ public class SysUserController extends BasePlusController {
      */
     //@SaCheckPermission("")
     @GetMapping("/list")
+    @ApiOperation(value = "查询列表-不分页")
     public R<List<SysUserVo>> list(SysUserDto dto) {
         List<SysUserVo> list = sysUserService.queryList(dto);
         return R.ok(list);
@@ -58,6 +63,7 @@ public class SysUserController extends BasePlusController {
     //@SaCheckPermission("")
     @Log(title = "", businessType = BusinessType.INSERT)
     @PostMapping
+    @ApiOperation(value = "新增")
     public R<Void> add(@RequestBody SysUserDto dto) {
         return toAjax(sysUserService.insertSysUser(dto));
     }
@@ -68,6 +74,7 @@ public class SysUserController extends BasePlusController {
     //@SaCheckPermission("")
     @Log(title = "", businessType = BusinessType.UPDATE)
     @PutMapping
+    @ApiOperation(value = "修改")
     public R<Void> edit(@RequestBody SysUserDto dto) {
         return toAjax(sysUserService.updateSysUser(dto));
     }
@@ -77,6 +84,7 @@ public class SysUserController extends BasePlusController {
      */
     //@SaCheckPermission("")
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "获取详细信息")
     public R<SysUserVo> getInfo(@NotNull(message = "ID不能为空") @PathVariable("id") Long id) {
         return R.ok(sysUserService.selectSysUserById(id));
     }
@@ -87,6 +95,7 @@ public class SysUserController extends BasePlusController {
     //@SaCheckPermission("")
     @Log(title = "", businessType = BusinessType.DELETE)
     @DeleteMapping("/{ids}")
+    @ApiOperation(value = "删除")
     public R<Void> remove(@NotNull(message = "ID不能为空") @PathVariable Long[] ids) {
         return toAjax(sysUserService.deleteSysUserByIds(ids));
     }
@@ -97,6 +106,7 @@ public class SysUserController extends BasePlusController {
     //@SaCheckPermission("")
     @Log(title = "", businessType = BusinessType.UPDATE)
     @PutMapping("/changeStatus")
+    @ApiOperation(value = "状态修改")
     public R<Void> changeStatus(@RequestBody SysUserDto dto) {
         return toAjax(sysUserService.updateSysUser(dto));
     }
--- com.yanshen.modules/com.yanshen.admin/src/main/java/com/yanshen/admin/domain/dto/SysUserDto.java
+++ com.yanshen.modules/com.yanshen.admin/src/main/java/com/yanshen/admin/domain/dto/SysUserDto.java
@@ -1,5 +1,6 @@
 package com.yanshen.admin.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,37 +18,43 @@ import java.util.Map;
 public class SysUserDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-            /**
-         * 
-         */
-        private Long id;
-        /**
-         * 
-         */
-        private String userName;
-        /**
-         * 
-         */
-        private String password;
+    /**
+     *
+     */
+    @ApiModelProperty(value = "ID")
+    private Long id;
+    /**
+     *
+     */
+    @ApiModelProperty(value = "用户名")
+    private String userName;
+    /**
+     *
+     */
+    @ApiModelProperty(value = "密码")
+    private String password;
 
     /**
      * 状态（0正常 1停用）
      */
+    @ApiModelProperty(value = "状态（0正常 1停用）")
     private String status;
 
     /**
      * 创建时间
      */
+    @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
     /**
      * 乐观锁
      */
+    @ApiModelProperty(value = "乐观锁")
     private Integer version;
 
     /**
      * 请求参数
      */
+    @ApiModelProperty(value = "请求参数")
     private Map<String, Object> params = new HashMap<>();
-    }
+}
